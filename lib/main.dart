901a309bd480c6567537a134536e6369517a7ab6
snehau259 <snehau259@gmail.com>
2023-03-06 18:20:15 +0530

    add. ListTile to home

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Taskly App',
-      theme: ThemeData(
-      
-        primarySwatch: Colors.blue,
-      ),
+     
       home: Home()
     );
   }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,10 +8,29 @@ class Home extends StatefulWidget {
 }
 
 class HomeState extends State<Home> {
-  
+  late double deviceWidth, deviceHeight;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    deviceWidth = MediaQuery.of(context).size.width;
+    deviceHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: deviceWidth * 0.15,
+        backgroundColor: Colors.cyan,
+      ),
+      body: ListView(children: [
+        ListTile(
+          title: Text(
+            'Eat well',
+            style:
+                TextStyle(decoration: TextDecoration.lineThrough, fontSize: 20),
+          ),
+          subtitle: Text(DateTime.now().toString()),
+          trailing: Icon(Icons.check_box_outline_blank_outlined,
+              color: Colors.red[500]),
+        )
+      ]),
+    );
   }
 }
